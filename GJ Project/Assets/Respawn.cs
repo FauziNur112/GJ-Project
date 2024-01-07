c30b484679d234b22c0b856af1992a327604ea1e
Fanur <105720266+FauziNur112@users.noreply.github.com>
2024-01-07 22:35:57 +0700

    Update Level Tiga

--- GJ Project/Assets/Respawn.cs
+++ GJ Project/Assets/Respawn.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Respawn : MonoBehaviour
 {
     [SerializeField] GameObject Player;
-    [SerializeField] SpawnOnLevelChange spawnOnLevelChange;
+    
     // Start is called before the first frame update
     void Start()
     {
